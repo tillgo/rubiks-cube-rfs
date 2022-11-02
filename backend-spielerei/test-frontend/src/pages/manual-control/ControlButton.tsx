07ae09d5d361f4929bdba3f91a651ca3fad3b2a2
Sigggii <simeon-holzapfel@gmx.de>
2022-11-02 18:53:54 +0100

    Keep Robo Moving on Hold. BTW Dark beste Serie und MULLLLLLMMMMMMMMM

--- backend-spielerei/test-frontend/src/pages/manual-control/ControlButton.tsx
+++ backend-spielerei/test-frontend/src/pages/manual-control/ControlButton.tsx
@@ -3,6 +3,8 @@ import {Button, IconButton} from "@mui/material";
 import {controlPanelConfig} from "./controlPanelConfig";
 import useWebSocket from "react-use-websocket";
 import {socketUrl} from "./ManualControlPage";
+import {useState} from "react";
+import sleep from "../../commons/sleep";
 
 type ControlButtonProps = {
     moveType: RobotMove
@@ -11,16 +13,22 @@ type ControlButtonProps = {
 
 const ControlButton = (props: ControlButtonProps) => {
     const config = controlPanelConfig[props.direction]
+    let buttonHold = false
 
     const {sendMessage} = useWebSocket(socketUrl);
 
-    const handleClick = () => {
-        sendMessage(JSON.stringify({moveType: props.moveType, moveDirection: props.direction}))
+    const handleMouseDown = async () => {
+        buttonHold = true
+        while (buttonHold) {
+            sendMessage(JSON.stringify({moveType: props.moveType, moveDirection: props.direction}))
+            await sleep(100)
+        }
     }
 
     return (
         <Button
-            onClick={handleClick}
+            onMouseDown={handleMouseDown}
+            onMouseUp={()=> buttonHold = false}
             sx={{width: "100%", height: "100%", border: "1px solid"}}
             color={"primary"}
             variant={"outlined"}
